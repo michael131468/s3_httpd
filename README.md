607baf533c2bcd52121844ad87ec6d855996b25a
Michael Ho <micho@redhat.com>
2022-04-27 01:02:44 +0200

    Add s3_httpd.py
    Update README to match.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # s3_httpd
 Small Python server to make S3 buckets accessible via HTTP requests
+
+## About
+
+There are many http servers that make S3 buckets browseable. This is yet another
+one. It's very minimal, it is a single Python script, simply run it and it works.
+
+## AWS Credentials
+
+Configure per boto3 instructions: 
+
+https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
---
+++ s3_httpd.py
@@ -0,0 +1,50 @@
+import http.server
+
+import boto3
+from botocore.errorfactory import ClientError
+
+class S3RequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Break up requested url into bucket, prefix, filename
+        print(f"GET: {self.path}")
+        path_parts = self.path.split("/")
+        if len(path_parts) < 2:
+            print(f"Invalid path: {path_parts}")
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'File not found')
+            return
+
+        bucket = path_parts[1]
+        prefix = "/".join(path_parts[2:-1])
+        filename = path_parts[-1]
+        print(f"bucket={bucket}, prefix={prefix}, filename={filename}")
+
+        # Get connection to s3
+        s3 = boto3.client('s3')
+
+        # Check file exists, if not return 404
+        try:
+            s3.head_object(Bucket=bucket, Key=f"{prefix}/{filename}")
+        except ClientError:
+            print(f"Not found: {bucket}/{prefix}/{filename}")
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'File not found')
+            return
+
+        # Try to serve s3 file
+        self.send_response(200)
+        self.end_headers()
+        try:
+            s3.download_fileobj(bucket, f"{prefix}/{filename}", self.wfile)
+        except:
+            # Write to socket
+            self.send_response(500)
+
+def main():
+    httpd = http.server.HTTPServer(('localhost', 8081), S3RequestHandler)
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    main()
