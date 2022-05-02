617c08a683322090e0d8377859025ae68dc0c4e2
Michael Ho <micho@redhat.com>
2022-05-02 13:38:54 +0200

    s3_httpd.py: add support for do_HEAD
    Allow requests that just want the headers and no actual file download to
    occur.

--- s3_httpd.py
+++ s3_httpd.py
@@ -6,21 +6,57 @@ import boto3
 from botocore.errorfactory import ClientError
 
 class S3RequestHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        # Break up requested url into bucket, prefix, filename
-        print(f"GET: {self.path}")
+    def split_path(self, path):
         path_parts = self.path.split("/")
         if len(path_parts) < 2:
+            raise Exception("Insufficient path parts")
+        bucket = path_parts[1]
+        prefix = "/".join(path_parts[2:-1])
+        filename = path_parts[-1]
+        return bucket, prefix, filename
+
+    def do_HEAD(self):
+        print(f"HEAD: {self.path}")
+
+        # Break up requested url into bucket, prefix, filename
+        try:
+            bucket, prefix, filename = self.split_path(self.path)
+            print(f"bucket={bucket}, prefix={prefix}, filename={filename}")
+        except:
+            print(f"Invalid path: {path_parts}")
+            self.send_response(404)
+            self.end_headers()
+            return
+
+        # Get connection to s3
+        s3 = boto3.client('s3')
+
+        # Check file exists, if not return 404
+        try:
+            s3.head_object(Bucket=bucket, Key=f"{prefix}/{filename}")
+        except ClientError:
+            print(f"Not found: {bucket}/{prefix}/{filename}")
+            self.send_response(404)
+            self.end_headers()
+            return
+
+        # Return 200 headers
+        self.send_response(200)
+        self.end_headers()
+
+    def do_GET(self):
+        print(f"GET: {self.path}")
+
+        # Break up requested url into bucket, prefix, filename
+        try:
+            bucket, prefix, filename = self.split_path(self.path)
+            print(f"bucket={bucket}, prefix={prefix}, filename={filename}")
+        except:
             print(f"Invalid path: {path_parts}")
             self.send_response(404)
             self.end_headers()
             self.wfile.write(b'File not found')
             return
-
-        bucket = path_parts[1]
-        prefix = "/".join(path_parts[2:-1])
-        filename = path_parts[-1]
-        print(f"bucket={bucket}, prefix={prefix}, filename={filename}")
 
         # Get connection to s3
         s3 = boto3.client('s3')
